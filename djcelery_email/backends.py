579968bc9c615e694251706d5dba9cc8cb65e20a
Daniel Axtens <daniel@axtens.net>
2014-02-20 08:27:46 +1100

    To/from dictionary functions to support celery's JSON serializer

--- djcelery_email/backends.py
+++ djcelery_email/backends.py
@@ -3,6 +3,18 @@ from django.core.mail.backends.base import BaseEmailBackend
 from djcelery_email.tasks import send_email
 
 
+def to_dict(message):
+    return {'subject': message.subject,
+            'body': message.body,
+            'from_email': message.from_email,
+            'to': message.to,
+            'bcc': message.bcc,
+            # ignore connection
+            'attachments': message.attachments,
+            'headers': message.extra_headers,
+            'cc': message.cc}
+
+
 class CeleryEmailBackend(BaseEmailBackend):
     def __init__(self, fail_silently=False, **kwargs):
         super(CeleryEmailBackend, self).__init__(fail_silently)
@@ -12,5 +24,5 @@ class CeleryEmailBackend(BaseEmailBackend):
         results = []
         kwargs['_backend_init_kwargs'] = self.init_kwargs
         for msg in email_messages:
-            results.append(send_email.delay(msg, **kwargs))
+            results.append(send_email.delay(to_dict(msg), **kwargs))
         return results
--- djcelery_email/tasks.py
+++ djcelery_email/tasks.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.mail import get_connection
+from django.core.mail import get_connection, EmailMessage
 
 from celery.task import task
 
@@ -14,13 +14,17 @@ TASK_CONFIG = {
 TASK_CONFIG.update(CONFIG)
 
 
+def from_dict(messagedict):
+    return EmailMessage(**messagedict)
+
+
 @task(**TASK_CONFIG)
 def send_email(message, **kwargs):
     logger = send_email.get_logger()
     conn = get_connection(backend=BACKEND,
                           **kwargs.pop('_backend_init_kwargs', {}))
     try:
-        result = conn.send_messages([message])
+        result = conn.send_messages([from_dict(message)])
         logger.debug("Successfully sent email message to %r.", message.to)
         return result
     except Exception as e:
